5eef3b965def4443bf0a76ae97caf49df2bdb51e
RajveerSodhi <rajveersodhi03@gmail.com>
2024-06-17 12:45:35 -0700

    Initial Commit

---
+++ FlightBar/FlightBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlightBarApp.swift
+//  FlightBar
+//
+//  Created by Rajveer Sodhi on 2024-06-17.
+//
+
+import SwiftUI
+
+@main
+struct FlightBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
