568a20fc3ca7cb18f86c0df6876322b7d7ade417
RajveerSodhi <rajveersodhi03@gmail.com>
2024-06-18 02:39:33 -0700

    added basic framework

--- FlightBar/FlightBarApp.swift
+++ FlightBar/FlightBarApp.swift
@@ -1,36 +1,56 @@
-//
-//  FlightBarApp.swift
-//  FlightBar
-//
-//  Created by Rajveer Sodhi on 2024-06-17.
-//
+// Allow to nickname flights and show nickname instead of flight number. e.g. - Juhi's Flight
+
+
+
 
 import SwiftUI
 
 @main
 struct FlightBarApp: App {
+    @StateObject private var flightViewModel = FlightViewModel()
+
     var body: some Scene {
-        
         MenuBarExtra {
             DetailsView()
+                .environmentObject(flightViewModel)
         } label: {
             MenuBarView()
+                .environmentObject(flightViewModel)
         }
         .menuBarExtraStyle(.window)
-        
-//        WindowGroup {
-//            ContentView()
-//        }
     }
-    
-    @ViewBuilder
-    func DetailsView() -> some View {
-        Image(systemName: "switch.2")
+}
+
+struct DetailsView: View {
+    @EnvironmentObject var flightViewModel: FlightViewModel
+
+    var body: some View {
+        VStack {
+            if let flight = flightViewModel.flight {
+                Text("Flight Number: \(flight.flight.iata)")
+                Text("ETA: \(flight.arrival.estimated)")
+                // Uncomment or adjust these as per the available data
+                // Text("Current Speed: \(flight.live?.speedHorizontal ?? 0) km/h")
+                // Text("Distance Left: \(flight.arrival.distanceLeft ?? 0) km")
+            } else {
+                Text("Loading flight details...")
+            }
+        }
+        .padding()
+        .onAppear {
+            flightViewModel.fetchFlightDetails()
+        }
     }
-    
-    @ViewBuilder
-    func MenuBarView() -> some View {
-        Image(systemName: "switch.2")
+}
+
+struct MenuBarView: View {
+    @EnvironmentObject var flightViewModel: FlightViewModel
+
+    var body: some View {
+        if let flight = flightViewModel.flight {
+            Text("Flight: \(flight.flight.iata), Status: \(flight.flightStatus)")
+        } else {
+            Text("Click to Reload")
+        }
     }
-    
 }
