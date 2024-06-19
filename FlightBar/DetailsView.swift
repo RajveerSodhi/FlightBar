6a01f40d418593765a9978497210d2cdf3fb8749
RajveerSodhi <rajveersodhi03@gmail.com>
2024-06-19 12:13:41 -0700

    added aesthetic changes to departure and arrival sections

--- FlightBar/DetailsView.swift
+++ FlightBar/DetailsView.swift
@@ -12,6 +12,33 @@ struct DetailsView: View {
     @State private var flightNumber: String = ""
     @EnvironmentObject var flightViewModel: FlightViewModel
     
+    func fixAirportName(name: String) -> String {
+        let words = name.split(separator: " ")
+        let newNameArray: [String] = words.map { word in
+            if word.uppercased() == "INTERNATIONAL" {
+                return "Intl"
+            } else {
+                return String(word)
+            }
+        }
+        let newName = newNameArray.joined(separator: " ")
+        
+        return newName
+    }
+    
+    func fixTime(dateTime: String) -> String {
+        
+        if dateTime == "N/A" {
+            return dateTime
+        }
+        
+        var time = dateTime.split(separator: "T")[1]
+        time = time.split(separator: "+")[0]
+        let newTime = time.dropLast(3)
+        
+        return String(newTime)
+    }
+    
     var body: some View {
         VStack(alignment:.center, spacing: 10) {
             if let flight = flightViewModel.flight {
@@ -19,14 +46,28 @@ struct DetailsView: View {
                 let flightPos = CLLocationCoordinate2D(latitude: flight.live?.latitude ?? 0, longitude: flight.live?.longitude ?? 0)
                 @State var camera: MapCameraPosition = .region(MKCoordinateRegion(center: flightPos, latitudinalMeters: 500000, longitudinalMeters: 500000))
                 
+                let airline = flight.airline.name.capitalized
+                let status = flight.flightStatus.capitalized
+                let flightNo = flight.flight.iata.uppercased()
+                let departureAirportCode = flight.departure.iata.uppercased()
+                let departureAirport = fixAirportName(name: flight.departure.airport)
+                let arrivalAirportCode = flight.arrival.iata.uppercased()
+                let arrivalAirport = fixAirportName(name: flight.arrival.airport)
+                let departureScheduled = fixTime(dateTime: flight.departure.scheduled ?? "N/A")
+                let arrivalScheduled = fixTime(dateTime:flight.arrival.scheduled ?? "N/A")
+                let departureActual = fixTime(dateTime:flight.departure.actual ?? "N/A")
+                let arrivalActual = fixTime(dateTime:flight.arrival.actual ?? "N/A")
+                let departureEstimated = fixTime(dateTime:flight.departure.estimated ?? "N/A")
+                let arrivalEstimated = fixTime(dateTime:flight.arrival.estimated ?? "N/A")
+                
                 VStack(spacing: 5) {
-                    Text("\(flight.airline.name) \(flight.flight.iata)")
+                    Text("\(airline) - \(flightNo)")
                         .font(.headline)
-                    Text((flight.flightStatus))
+                    Text((status))
                         .font(.subheadline)
                     
                     Map(position: $camera) {
-                        Annotation((flight.flight.iata), coordinate: flightPos) {
+                        Annotation((flightNo), coordinate: flightPos) {
                             Image(systemName: "airplane")
                                 .foregroundStyle(.white)
                                 .padding()
@@ -34,27 +75,112 @@ struct DetailsView: View {
                                 .cornerRadius(10)
                         }
                     }
-                        .frame(height:120)
+                        .frame(height:140)
                         .cornerRadius(15)
                     
-                    Divider()
+                    Spacer()
                     
-                    Text("Departure")
-                        .font(.headline)
+                    HStack {
+                        VStack {
+                            Text(departureAirportCode)
+                                .font(.headline)
+                            Text(departureAirport)
+                                .font(.subheadline)
+                                .foregroundColor(Color(hex: "#787878"))
+                                .frame(width: 100)
+                            
+                            Spacer()
+                            
+                            HStack {
+                                Spacer()
+                                
+                                VStack {
+                                    Text(departureScheduled)
+                                    Text("SCHD")
+                                        .font(.caption)
+                                        .foregroundColor(Color(hex: "#787878"))
+                                }
+                                
+                                Spacer()
+                                
+                                VStack {
+                                    if ((departureActual) != "N/A") {
+                                        Text(departureActual)
+                                        Text("ACTL")
+                                            .font(.caption)
+                                            .foregroundColor(Color(hex: "#787878"))
+                                    }
+                                    else {
+                                        Text(departureEstimated)
+                                        Text("ESTD")
+                                            .font(.caption)
+                                            .foregroundColor(Color(hex: "#787878"))
+                                    }
+                                }
+                                
+                                Spacer()
+                            }
+                        }
+                        .foregroundColor(.black)
+                        .padding()
+                        .background(Rectangle()
+                            .foregroundColor(.white)
+                            .cornerRadius(15)
+                        )
+                        
+                        Spacer()
+                        Image(systemName: "arrow.forward")
+                        Spacer()
+                        
+                        VStack {
+                            Text(arrivalAirportCode)
+                                .font(.headline)
+                            Text(arrivalAirport)
+                                .font(.subheadline)
+                                .foregroundColor(Color(hex: "#787878"))
+                                .frame(width: 100)
+                            
+                            Spacer()
+                            
+                            HStack {
+                                Spacer()
+                                
+                                VStack {
+                                    Text(arrivalScheduled)
+                                    Text("SCHD")
+                                        .font(.caption)
+                                        .foregroundColor(Color(hex: "#787878"))
+                                }
+                                
+                                Spacer()
+                                
+                                VStack {
+                                    if ((arrivalActual) != "N/A") {
+                                        Text(arrivalActual)
+                                        Text("ACTL")
+                                            .font(.caption)
+                                            .foregroundColor(Color(hex: "#787878"))
+                                    }
+                                    else {
+                                        Text(arrivalEstimated)
+                                        Text("ESTD")
+                                            .font(.caption)
+                                            .foregroundColor(Color(hex: "#787878"))
+                                    }
+                                }
+                                
+                                Spacer()
+                            }
+                        }
+                        .foregroundColor(.black)
+                        .padding()
+                        .background(Rectangle()
+                            .foregroundColor(.white)
+                            .cornerRadius(15)
+                        )
+                    }
                     
-                    Text("Airport: \(flight.departure.airport)")
-                    Text("Time: \(flight.departure.scheduled ?? "N/A")")
-                    
-                    Divider()
-                    
-                    Text("Arrival")
-                        .font(.headline)
-                    
-                    Text("Airport: \(flight.arrival.airport)")
-                    Text("Time: \(flight.arrival.scheduled ?? "N/A")")
-                    Text("Gate: \(flight.arrival.gate ?? "N/A")")
-                    Text("Baggage Claim: \(flight.arrival.baggage ?? "N/A")")
-                    Text("Delay: \(flight.arrival.delay ?? 0) minutes")
+                    Spacer()
                     
                     Divider()
                     
@@ -73,8 +199,6 @@ struct DetailsView: View {
                 Text("Loading flight details...")
             }
             
-            Divider()
-            
             TextField("Enter Flight Number", text: $flightNumber)
                 .textFieldStyle(RoundedBorderTextFieldStyle())
                 .padding([.leading, .trailing])
@@ -87,15 +211,10 @@ struct DetailsView: View {
             .padding()
         }
         .padding()
-        .frame(width: 360)
+        .frame(width: 420)
         .onAppear {
             flightNumber = flightViewModel.storedFlightNumber
             flightViewModel.fetchFlightDetails(for: flightNumber)
         }
     }
-}
-
-
-#Preview {
-    DetailsView()
 }
